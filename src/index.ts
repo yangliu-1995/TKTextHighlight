9f1e3157ded05b86cee4602f84adef50248ac3c8
yangliu <xuexiao@cisco.com>
2025-11-17 17:43:07 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import TextHighlighter from './TextHighlighter';
 export default TextHighlighter;
 export * from './types';
 
+// 自动初始化全局实例
+if (typeof window !== 'undefined') {
+  (window as any).tkHighlighter = new TextHighlighter();
+}
